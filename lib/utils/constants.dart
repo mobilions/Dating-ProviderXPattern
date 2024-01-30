c36577814773deb89d3d3c59d62c97ca890617aa
mobilions <mobilionsteam@gmail.com>
2024-01-30 12:18:23 +0530

    Minor changes

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,11 +1,5 @@
 class Constants {
   Constants._();
-
-  static String mapUrl(lat, lng, label, scale) =>
-      "https://maps.googleapis.com/maps/api/staticmap?markers=icon:https://www.famooshed.com/pin2.png%7Clabel:$label%7C%7C$lat,$lng&zoom=16&size=400x225&key=AIzaSyBlPrKDHq4CtetPgQivrBqsH87JkvLGtQw&scale=$scale";
-
-  static const String kGoogleMap = 'AIzaSyBlPrKDHq4CtetPgQivrBqsH87JkvLGtQw';
-
   static const accessToken = 'accessToken';
   static const userId = 'userId';
   static const userName = 'userName';
